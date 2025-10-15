789d8857c0907331e3b2f0fdab470955162976d7
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-10-15 21:55:24 +0800

    Add initial TipButtons component functionality

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import DollarIcon from "../public/icon-dollar.svg"
 import PersonIcon from "../public/icon-person.svg"
 import InputField from "./components/InputField";
 import OutputField from "./components/OutputField";
+import TipButtons from "./components/TipButtons";
 
 export default function Home() {
   return (
@@ -16,28 +17,26 @@ export default function Home() {
         <div className="bg-main-container-color max-w-4x1 rounded-3xl p-8 mt-10">
           <div className="grid grid-cols-2 gap-12">
             <div className="flex flex-col col-start-1">
+              
               <p className="text-m text-label-text-color">Bill</p>
               <InputField icon={DollarIcon} />
-
-              <div className="grid grid-cols-3 grid-rows-2 gap-4 mt-10"> {/* Button Component to be Implemented */}
-                <button className="bg-button-color p-2.5">5%</button>
-                <button className="bg-button-color p-2.5">10%</button>
-                <button className="bg-button-color p-2.5">15%</button>
-                <button className="bg-button-color p-2.5">25%</button>
-                <button className="bg-button-color p-2.5">50%</button>
-                <button className="bg-button-color p-2.5">Custom</button>
-              </div>
-
+              
+              <TipButtons />
+              
               <p className="text-m text-label-text-color mt-10">Number of People</p>
               <InputField icon={PersonIcon} />
+            
             </div>
             <div className="bg-output-container-color flex flex-col col-start-2">
+              
               <div className="pt-4"></div> 
+              
               <OutputField label="Tip Amount" amount={0}/>
               <OutputField label="Total" amount={0} />
 
               <div className="bg-output-color text-output-container-color flex max-w-50 w-50 p-4 justify-center items-center self-center mt-16 rounded-lg">
                 <button className="text-reset-text-color">RESET</button>
+              
               </div>
             </div>
           </div>
