3291b5061905cc1f7436387f1381013c3b6b7e18
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-10-15 10:57:53 +0800

    Optimize reset button and icon positioning

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,12 @@ export default function Home() {
         <div className="bg-main-container-color max-w-4x1 rounded-3xl p-8 mt-10">
           <div className="grid grid-cols-2 gap-12">
             <div className="flex flex-col col-start-1">
-              <p className="text-2xl text-label-text-color">Bill</p>
+              <p className="text-m text-label-text-color">Bill</p>
               <div className="bg-input-field-color flex flex-row border-black w-full justify-between"> {/*Input Field Component*/}
                 <Image
                   src={DollarIcon}
                   alt=""
+                  className="ml-2.5"
                 />
                 <input className="ml-4 text-input-text-color text-right text-4x1" 
                   type="text"
@@ -35,11 +36,12 @@ export default function Home() {
                 <button className="bg-button-color p-2.5">Custom</button>
               </div>
 
-              <p className="text-2xl text-label-text-color mt-10">Number of People</p>
+              <p className="text-m text-label-text-color mt-10">Number of People</p>
               <div className="bg-input-field-color flex flex-row border-black w-full justify-between"> {/*Input Field Component*/}
                 <Image
                   src={PersonIcon}
                   alt=""
+                  className="ml-2.5"
                 />
                 <input className="ml-4 text-input-text-color text-right text-4x1" 
                   type="text"
@@ -71,7 +73,7 @@ export default function Home() {
                 </div>
               </div> {/* Output Field Component to Implement */}
 
-              <div className="bg-output-color text-output-container-color flex p-4 justify-center items-center mt-16">
+              <div className="bg-output-color text-output-container-color flex max-w-50 w-50 p-4 justify-center items-center self-center mt-16 rounded-lg">
                 <button className="text-reset-text-color">RESET</button>
               </div>
             </div>
